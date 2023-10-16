884ce68182ecef0ea17844c7789ea7266fd364df
junyub1103 <dlwnsduq14@naver.com>
2023-10-16 21:44:57 +0900

    ⚙️chore: react-scroll, react-scroll-motion 제거, gsap 설치

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,12 +1,18 @@
 import Layout from "../../components/PageLayout.tsx";
 import React from "react";
+import {ScrollContainer} from "react-scroll-motion";
+import Introduction from "../../lib/scrollMotion/Introduction.tsx";
+import ProductInsight from "../../lib/scrollMotion/ProductInsight.tsx";
 
 
 const HomePage: React.FC = () => {
   return(
       <>
           <Layout>
-              <p>Home Page</p>
+              <ScrollContainer>
+                  <Introduction/>
+                  <ProductInsight/>
+              </ScrollContainer>
           </Layout>
       </>
   )
