654694232616876479979f16369c3ac6c21c14b7
junyub1103 <dlwnsduq14@naver.com>
2023-10-21 12:36:05 +0900

    ⚙️chore: smooth-scroll-bar 제거

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,19 +1,15 @@
-import Layout from "../../components/PageLayout.tsx";
 import React from "react";
-import {ScrollContainer} from "react-scroll-motion";
-import Introduction from "../../lib/scrollMotion/Introduction.tsx";
-import ProductInsight from "../../lib/scrollMotion/ProductInsight.tsx";
+import PageLayout from "../../components/PageLayout.tsx";
 
 
 const HomePage: React.FC = () => {
+
+
   return(
       <>
-          <Layout>
-              <ScrollContainer>
-                  <Introduction/>
-                  <ProductInsight/>
-              </ScrollContainer>
-          </Layout>
+          <PageLayout>
+
+          </PageLayout>
       </>
   )
 }
