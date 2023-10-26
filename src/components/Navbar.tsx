9cbd8e4e0ecf38a4278197dc89d3a7491977ebf2
junyub1103 <dlwnsduq14@naver.com>
2023-10-26 09:27:57 +0900

    🎨design: main ProductType section 작업, NavBar 로고 이미지 삽입

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import * as React from "react";
 import {Link} from "react-router-dom";
+import LOGO from "../asset/img/familyFarm_logo2.jpeg"
 
 const NavBarContainer = styled.nav`
   position:fixed;
@@ -16,9 +17,15 @@ const NavBarContainer = styled.nav`
   border-radius: 0.5em;
 `
 const NavBarLogo = styled(Link)`
+  margin-left:3em;
   padding-left:3em;
   flex: 0 0 auto;
   font-size: 2em;
+  background: url(${LOGO}) no-repeat;
+  background-size: contain;
+  height:80px;
+  width:120px;
+  //border:1px solid black;
 `
 
 const NavBarUl = styled.ul`
@@ -48,7 +55,7 @@ const NavBar: React.FC = () => {
         <>
             <NavBarContainer>
                 <NavBarLogo to="/">
-                    로고
+
                 </NavBarLogo>
                 <NavBarUl>
                     {/* Link (react-router-dom) 을 사용할때엔 to 를 반드시 작성해야 컴파일 에러가 없습니다. */}
--- src/page/Home/ProductType.tsx
+++ src/page/Home/ProductType.tsx
@@ -1,27 +1,30 @@
 import React, {useEffect, useState} from "react";
 import styled, {keyframes} from "styled-components";
+import {Autoplay, Virtual, Navigation} from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css"
 
 const slideIn = keyframes`
   from {
     opacity: 0;
-    transform: translateX(150px); /* 초기 위치 설정 */
+    transform: translateY(150px); /* 초기 위치 설정 */
   }
   to {
     opacity: 1;
-    transform: translateX(0); /* 최종 위치 설정 */
+    transform: translateY(0); /* 최종 위치 설정 */
   }
 `;
 
-const slideInLeft = keyframes`
-  from {
-    opacity: 0;
-    transform: translateX(-150px); /* 초기 위치 설정 */
-  }
-  to {
-    opacity: 1;
-    transform: translateX(0); /* 최종 위치 설정 */
-  }
-`;
+// const slideInLeft = keyframes`
+//   from {
+//     opacity: 0;
+//     transform: translateY(-150px); /* 초기 위치 설정 */
+//   }
+//   to {
+//     opacity: 1;
+//     transform: translateY(0); /* 최종 위치 설정 */
+//   }
+// `;
 
 
 const ProductTypeContainer = styled.section`
@@ -32,15 +35,36 @@ const ProductTypeContainer = styled.section`
   justify-content:center;
 `
 
+const ProductTypeCard = styled.div`
+  width: 40em;
+  padding: 4em 0;
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  gap:2em;
+  border-radius: 1em;
+  border: 1px solid black;
+  
+`
+
 const ProductTypeTitle = styled.h2`
-  font-size: 5em;
+  font-size: 4em;
   color: #f39e9e;
   text-align: center;
-  animation: ${slideInLeft} 3s ease forwards;
+`
+
+const ProductTypeImg = styled.div`
+  width:100%;
+  height:20em;
+  background-color:gray;
+`
+
+const ProductTypeDesc = styled.p`
+  font-size:1.4em;
 `
 
 const ProductTypeButton = styled.button`
-  padding:1em;
+  padding:1em 10em;
   margin-top:2em;
   background-color: red;
   color:white;
@@ -53,6 +77,32 @@ const ProductTypeButton = styled.button`
     background-color:red;
     color:black;
   }
+`
+
+const CustomSwiper = styled(Swiper)`
+  width:100%;
+  padding:5em;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  animation: ${slideIn} 3s ease forwards;
+  
+  .swiper-button-prev{
+    &:after{
+      color:red;
+      font-size:2em;
+    }
+  }
+  .swiper-button-next{
+    &:after{
+      color:red;
+      font-size:2em;
+    }
+  }
+`
+
+const CustomSwiperSlide = styled(SwiperSlide)`
+  
 `
 
 
@@ -88,10 +138,35 @@ const ProductType: React.FC = () => {
                 {
                     isVisible ?
                         <>
-                            <ProductTypeTitle>
-                                Welcome Family Farm
-                            </ProductTypeTitle>
-                            <ProductTypeButton>Buy Now !</ProductTypeButton>
+                            <CustomSwiper
+                                modules={[Autoplay,Virtual,Navigation]}
+                                autoplay={{
+                                    delay:5000,
+                                }}
+                                spaceBetween={20}
+                                slidesPerView={3}
+                                virtual
+                                navigation
+                            >
+                                {
+                                    [1,2,3,4,5,6,7,8,9,10].map((value: number) => {
+                                        return(
+                                            <CustomSwiperSlide>
+                                                <ProductTypeCard>
+                                                    <ProductTypeTitle>
+                                                        {value + "사과"}
+                                                    </ProductTypeTitle>
+                                                    <ProductTypeImg></ProductTypeImg>
+                                                    <ProductTypeDesc>
+                                                        7월 11월엔 00사과 판매
+                                                    </ProductTypeDesc>
+                                                </ProductTypeCard>
+                                            </CustomSwiperSlide>
+                                        )
+                                    })
+                                }
+                            </CustomSwiper>
+                            <ProductTypeButton>구매하러 가기</ProductTypeButton>
                         </>
                         :
                         null
