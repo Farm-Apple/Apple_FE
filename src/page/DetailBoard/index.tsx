979c2e1628eb1e042a877cfc5dbfb1cc435403ff
Yangjisung <ygji6406@gmail.com>
2024-01-18 23:31:13 +0900

    feat : getProductDetail 패칭 코드 작성

--- src/page/DetailBoard/index.tsx
+++ src/page/DetailBoard/index.tsx
@@ -1,5 +1,14 @@
+import { useEffect } from 'react';
+import { getProductDetail } from '../../api/auth/product';
+
 export default function DetailBoardPage() {
-  return (
-    <p>DetailBoard Page</p>
-  )
+  const Detail = async () => {
+    const data = await getProductDetail(2);
+    console.log(data.data);
+    return data;
+  };
+  useEffect(() => {
+    Detail();
+  }, []);
+  return <p>DetailBoard Page</p>;
 }
