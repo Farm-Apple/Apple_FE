be996fa63885af1c988864116c44620fe87789aa
junyub1103 <dlwnsduq14@naver.com>
2024-01-24 23:23:58 +0900

    🎨design: 장바구니 아이콘 추가, 호버 이펙트 추가

--- src/page/Product/ProductList.tsx
+++ src/page/Product/ProductList.tsx
@@ -3,12 +3,13 @@ import styled from "styled-components";
 import {GetProductList} from "../../api/auth/auth.ts";
 import mainApple from "../../asset/img/main_apple.jpg"
 import {Link} from "react-router-dom";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 
 const ProductListContainer = styled.section`
   display: flex;
   justify-content: center;
-  background-color: #efefef;
+  background-color: white;
   padding-top: 20rem;
 `
 
@@ -17,51 +18,71 @@ const ProductListUl = styled.ul`
   display:flex;
   justify-content: center;
   padding-bottom: 10rem;
-  gap:3rem;
+  gap:7rem 3rem;
   flex-wrap:wrap;
 `
 
 const ProductListLi = styled(Link)`
-  width:30rem;
-  height:30rem;
+  width: calc(100% / 4 - 9rem);
   display:flex;
+  position:relative;
   flex-direction:column;
   justify-content:center;
-  align-items:center;
+  align-items:flex-start;
   border-radius: 2rem;
-  padding:2rem;
+  padding:2rem 7rem;
   background-color:white;
-  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  //box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
   cursor:pointer;
-  transition:.3s;
   color:black;
-  &:hover{
-    transform:scale(1.1);
+  &:hover {
+    & > div{
+      //transform:scaleX(-1);
+      opacity:0.2;
+    }
+    & .MuiSvgIcon-root{
+      opacity:1;
+    }
   }
 `
-const ProductListTitle = styled.h3`
-  font-size:2.4rem;
-  text-align:center;
-`
-
 const ProductListImg = styled.div`
   width:25rem;
-  height:20rem;
+  height:25rem;
+  align-self:center;
   background:url(${mainApple}) no-repeat center center;
-  background-size: contain;
-`
-const ProductDetailButton = styled.button`
-  padding: 1rem 2rem;
-  background-color:white;
+  background-size: cover;
   border-radius:1rem;
-  color:black;
-  border:1px solid black;
-  
+  transition:.3s;
+  overflow:hidden;
+  margin-bottom:2rem;
+`
+
+const ProductListTitle = styled.h3`
+  font-size:2rem;
+  text-align:center;
+  font-weight: normal;
+  margin-bottom:.5rem;
+`
+const ProductListPrice = styled.h4`
+  font-size:2.6rem;
+`
+
+const ProductHashTag = styled.h5`
+  padding:.3rem 1rem;
+  background-color:#FFBB64;
+  border-radius:2rem;
+  font-size:1.2rem;
+  margin-bottom:1rem;
+`
+const ProductHasTagNone = styled.div`
+  padding: .3rem 1rem;
+  margin-bottom:1rem;
 `
 interface AppleListType{
     id: number;
     product_name: string;
     price: number;
+    detail: string;
     weight: string;
     created_at: string;
     updated_at: string;
@@ -84,10 +105,18 @@ const ProductList:React.FC = () => {
                 {
                     AppleList.map((item) => {
                         return(
-                            <ProductListLi to="/ProductOrder">
-                                <ProductListTitle>{item.product_name}</ProductListTitle>
+                            <ProductListLi to="/ProductOrder" key={item.id}>
                                 <ProductListImg></ProductListImg>
-                                <ProductDetailButton>상세보기</ProductDetailButton>
+                                <ShoppingCartIcon sx={{fontSize:"6rem",color:"#739072",position:"absolute",top:"30%",left:"50%",transform:"translateX(-50%)",opacity:"0",transition:"0.3s"}}/>
+                                {
+                                    item.detail ?
+                                        <ProductHashTag>#{item.detail}</ProductHashTag>
+                                        :
+                                        <ProductHasTagNone></ProductHasTagNone>
+                                }
+
+                                <ProductListTitle>{item.product_name}</ProductListTitle>
+                                <ProductListPrice>{item.price.toLocaleString()}원</ProductListPrice>
                             </ProductListLi>
                         )
                     })
