d7eb8e2aba304f35ebe3977f6fa9c279454698ac
양지성 <yangjiseong@yangjiseongs-MacBook-Air.local>
2024-03-13 12:20:14 +0900

    style : CartItem layout 작업

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -4,23 +4,46 @@ import styled from "styled-components";
 
 const CartItemContainer = styled.ul``;
 const CartItemList = styled.li`
+  position: relative;
   border: 1px solid #000;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding: 20px 0;
 `;
 const ProductInfoContainer = styled.div``;
-const ProductInfoWrapper = styled.div``;
+const ProductInfoWrapper = styled.div`
+  display: flex;
+  gap: 36px;
+  margin-left: 90px;
+`;
 const ProductDetailInfo = styled.div``;
-const ProductImage = styled.img``;
-const ProductTotalPrice = styled.div``;
+const ProductImage = styled.img`
+  border: 1px solid #000;
+  border-radius: 10%;
+  width: 160px;
+  height: 160px;
+`;
+const ProductTotalPrice = styled.div`
+  margin-right: 100px;
+`;
 const Price = styled.div``;
+const Input = styled.input`
+  position: absolute;
+  top: 50%;
+  left: 28px;
+`;
+const DeleteButton = styled.button`
+  position: absolute;
+  top: 18px;
+  right: 18px;
+`;
 
 const CartItem: React.FC = () => {
   return (
     <CartItemContainer>
       <CartItemList>
-        <input type="checkbox" />
+        <Input type="checkbox" />
         <ProductInfoContainer>
           <ProductInfoWrapper>
             <ProductImage />
@@ -36,7 +59,7 @@ const CartItem: React.FC = () => {
           <Price>30,000</Price>
           <button>주문하기</button>
         </ProductTotalPrice>
-        <button>삭제</button>
+        <DeleteButton>삭제</DeleteButton>
       </CartItemList>
     </CartItemContainer>
   );
