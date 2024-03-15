190aa76c9902c60e9768a79eadc86794864713ff
junyub1103 <dlwnsduq14@naver.com>
2024-03-15 15:48:16 +0900

    🎨design: 배송정보, 결제수단 design 작업(0315)

--- src/page/OrderComplete/PaymentMethod.tsx
+++ src/page/OrderComplete/PaymentMethod.tsx
@@ -39,6 +39,80 @@ const PaymentLabel = styled.label`
         font-size:1.6rem;
     }
 `
+const ResultPayInfoTitle = styled(PaymentMethodTitle)`
+    border-bottom: none;
+`
+const ResultPayInfoBox = styled.div`
+    border:2px solid green;
+    border-radius: 2rem;
+    overflow:hidden;
+`
+const ResultPayInfoUl = styled.ul`
+    display:flex;
+    flex-direction:column;
+    padding:2rem;
+    gap:2rem;
+    border-bottom:1px solid #F2F2F2;
+    
+    &.none{
+        border-bottom:none;
+    }
+`
+const ResultPayInfoLi = styled.li`
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+    font-size:1.6rem;
+    
+    & p span{
+        font-size: 2rem;
+        font-weight:bold;
+        margin-right:.2rem;
+    }
+    & p span.red{
+        color:red;
+        font-size:2.4rem;
+    }
+`
+const ResultPayConfirm = styled.div`
+    padding: 3rem 2rem;
+    background-color: #d9d9d9;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 2rem;
+
+    & label {
+        display: flex;
+        align-items: center;
+        font-size: 1.8rem;
+        justify-content: center;
+        gap: 2rem;
+    }
+
+    & label input {
+        width: 1.8rem;
+        height: 1.8rem;
+    }
+
+    & button {
+        padding: 2rem;
+        width: 15rem;
+        border-radius: 2rem;
+        background-color: #C4C4C4;
+        color: white;
+        font-size: 2rem;
+        cursor: pointer;
+        transition: .3s;
+    }
+
+    & button:hover {
+        background-color: red;
+        color: white;
+    }
+
+`
 
 export default function PaymentMethod() {
     return (
@@ -70,7 +144,36 @@ export default function PaymentMethod() {
                     </PaymentLabelWrap>
                 </PaymentMethodWrap>
                 <ResultPayInfo>
-
+                    <ResultPayInfoTitle>최종결제 정보</ResultPayInfoTitle>
+                    <ResultPayInfoBox>
+                        <ResultPayInfoUl>
+                            <ResultPayInfoLi>
+                                <p>- 상품금액</p>
+                                <p><span>46,500</span>원</p>
+                            </ResultPayInfoLi>
+                            <ResultPayInfoLi>
+                                <p>- 할인금액</p>
+                                <p><span>46,500</span>원</p>
+                            </ResultPayInfoLi>
+                            <ResultPayInfoLi>
+                                <p>- 배송비</p>
+                                <p><span>46,500</span>원</p>
+                            </ResultPayInfoLi>
+                        </ResultPayInfoUl>
+                        <ResultPayInfoUl className="none">
+                            <ResultPayInfoLi>
+                                <p>- 결제금액</p>
+                                <p><span className="red">46,500</span>원</p>
+                            </ResultPayInfoLi>
+                        </ResultPayInfoUl>
+                        <ResultPayConfirm>
+                            <label>
+                                <input type="checkbox"/>
+                                <p>주문내용을 확인하였으며, 정보제공 등에 동의합니다.</p>
+                            </label>
+                            <button>결제하기</button>
+                        </ResultPayConfirm>
+                    </ResultPayInfoBox>
                 </ResultPayInfo>
             </PaymentMethodContainer>
         </>
